f662e49e1ffe2569d051b299bc07d32fcbf90a34
nathanjh-28 <nathan@nathanjharris.com>
2021-01-21 12:17:56 -0700

    ch21 complete

--- laracasts-php-course/core/bootstrap.php
+++ laracasts-php-course/core/bootstrap.php
@@ -4,8 +4,8 @@ $app = [];
 
 $app['config'] = require 'config.php';
 
-require 'core/Router.php';
-require 'core/database/Connection.php';
-require 'core/database/QueryBuilder.php';
-require 'core/Request.php';
+// require 'core/Router.php';
+// require 'core/database/Connection.php';
+// require 'core/database/QueryBuilder.php';
+// require 'core/Request.php';
 $app['database'] = new QueryBuilder(Connection::make($app['config']['database']));
---
+++ laracasts-php-course/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '9965cea0548f3a4b0dc792c3880b0dfdad7a8b7b',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9965cea0548f3a4b0dc792c3880b0dfdad7a8b7b',
+    ),
+  ),
+);
